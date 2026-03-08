aeb48fc7db64bd3d60cd76cb16eb83ce88480e7e
nejib120-netizen <nejib120@gmail.com>
2026-03-08 13:59:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ async function getGroqResponse(senderId, userText) {
       {
    model: "llama-3.3-70b-versatile",
         messages: formattedMessages,
-        temperature: 0.7,
+        temperature: 0.6,
         max_tokens: 1200
       },
       {
